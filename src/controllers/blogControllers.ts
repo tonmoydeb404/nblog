2e7a38fa3b3f499366bd10cd878c704e5270c701
Tonmoy Deb <tonmoydeb404@gmail.com>
2023-04-25 13:27:41 +0600

    rich text editor added

--- src/controllers/blogControllers.ts
+++ src/controllers/blogControllers.ts
@@ -1,8 +1,13 @@
-import { NextFunction, Request, Response } from "express";
+import { Post } from "../models/Post";
+import asyncWrapper from "../utils/asyncWrapper";
 
-export const getBlogs = (req: Request, res: Response, next: NextFunction) => {
+export const getBlogs = asyncWrapper(async (_req, res) => {
+  const posts = await Post.find({});
+  res.locals.posts = posts;
   res.render("blog/home.ejs");
-};
-export const getBlog = (req: Request, res: Response, next: NextFunction) => {
+});
+export const getBlog = asyncWrapper(async (req, res) => {
+  const post = await Post.findById(req.params.id);
+  res.locals.post = post;
   res.render("blog/post.ejs");
-};
+});
