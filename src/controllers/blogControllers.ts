541d8484c43e7bddc6f3c59607291b44935c8e2c
Tonmoy Deb <tonmoydeb404@gmail.com>
2023-04-25 17:51:06 +0600

    rich text editor more opiton added

--- src/controllers/blogControllers.ts
+++ src/controllers/blogControllers.ts
@@ -8,6 +8,7 @@ export const getBlogs = asyncWrapper(async (_req, res) => {
 });
 export const getBlog = asyncWrapper(async (req, res) => {
   const post = await Post.findById(req.params.id);
+  res.locals.title = post.title;
   res.locals.post = post;
   res.render("blog/post.ejs");
 });
