bc9553c92cb7a654978178b0c78be9c27b5d2ab1
Tonmoy Deb <tonmoydeb404@gmail.com>
2023-04-27 00:15:17 +0600

    category page added

--- src/controllers/blogControllers.ts
+++ src/controllers/blogControllers.ts
@@ -1,4 +1,5 @@
 import createHttpError from "http-errors";
+import { Category } from "../models/Category";
 import { Post } from "../models/Post";
 import asyncWrapper from "../utils/asyncWrapper";
 
@@ -13,8 +14,17 @@ export const getBlog = asyncWrapper(async (req, res, next) => {
   const post = await Post.findOne({ slug: req.params.slug }).populate(
     "category"
   );
-  if (!post) return next(createHttpError(404, "Post not found"));
+  if (!post) return next(createHttpError(404, "Requested content not found"));
   res.locals.title = post.title;
   res.locals.data = post;
   res.render("blog/post.ejs");
+});
+export const getCategory = asyncWrapper(async (req, res, next) => {
+  const category = await Category.findOne({ slug: req.params.slug });
+  if (!category) return next();
+  const posts = await Post.find({ category: category.id });
+
+  res.locals.title = category.title;
+  res.locals.data = { category, posts };
+  res.render("blog/category.ejs");
 });
--- src/routers/blog.ts
+++ src/routers/blog.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { getBlog, getBlogs } from "../controllers/blogControllers";
+import { getBlog, getBlogs, getCategory } from "../controllers/blogControllers";
 import formatResponse from "../middlewares/formatResponse";
 
 const blogRouter = Router();
 
 blogRouter.get("/", formatResponse("Home"), getBlogs);
+blogRouter.get("/:slug", formatResponse("Category"), getCategory);
 blogRouter.get("/:slug", formatResponse("Post"), getBlog);
 
 export default blogRouter;
